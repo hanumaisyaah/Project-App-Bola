7dc4fcc6082ee86e32cff151fdc54288fb8561c7
hanumaisyaah <hanumaisyaah@gmail.com>
2021-10-10 20:56:35 +0700

    add widgets and fix bugs

--- lib/widgets/news.dart
+++ lib/widgets/news.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class news extends StatelessWidget {
   const news({
-    Key key,
+    Key? key,
   }) : super(key: key);
 
   @override
@@ -16,11 +16,11 @@ class news extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Image.network(''),
+          Image.network('https://asset.kompas.com/crops/3xTK4p6NP3PqdGM8mE_4Ay1PyRQ=/247x0:958x474/750x500/data/photo/2019/07/27/5d3ba98ca67ed.jpg'),
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: Center(
-              chiled: Text(
+              child: Text(
                 "Costa Mendekat Ke Palmeiras",
                 style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
               ),
@@ -30,7 +30,7 @@ class news extends StatelessWidget {
             height: 50, 
             color: Colors.purpleAccent,
             child: Row(
-              children[
+              children: [
                 Padding(
                   padding: const EdgeInsets.all(8.10),
                   child: Text("Transfer", style: TextStyle(fontSize:15),
